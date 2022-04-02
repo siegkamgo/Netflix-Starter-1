cc385021db46df16846b2ff53856792be4f19c90
Siegfried Kamgo <siegfriedkamgo04@gmail.com>
2022-04-02 20:22:09 +0300

    Adjustments to the Banner

---
+++ src/helpers/library.js
@@ -0,0 +1,20 @@
+import titanicS from "../images/titanicScene.png";
+import tita from "../images/tit.png";
+import movieLogo from "../images/movieLogo.png";
+
+
+export const movies = [
+    {
+        Movie: "",
+        Thumbnail: tita,
+        Scene: titanicS,
+        Logo: movieLogo,
+        Name: "Titanic",
+        Description: "Staring Kate Winslet and Leornado Dicaprio, The two are from different social classes. They fall in love after meeting aboard the ship, but it was not good for a rich girl to fall in love with a poor boy in 1912",
+        Genre: "Romance, Drama",
+        Actors: "Kate Winslet, Leonardo DiCaprio",
+        Year: "1998",
+        Duration: "3h 14min",
+    },
+
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Home.css";
+import { Logo } from '../images/Netflix';
+import {ConnectButton, Icon, TabList, Tab, Button} from "web3uikit";
+import { movies } from '../helpers/library';
 
 const Home = () => {
 
 return(
   <>
-  <div className="container">
-     <Link to="/player" className="link">Player Page</Link>
-  </div>
+    <div className='logo'>
+      <Logo />
+    </div>
+    <div className='connect'>
+    <Icon fill="#ffffff" size={24} svg="bell" />
+      <ConnectButton />
+    </div>
+    <div className="topBanner">
+      <TabList defaultActiveKey={1} tabStyle="bar">
+        <Tab tabKey={1} tabName={"Movies"}>
+
+          <div className='scene'>
+            <img src={movies[0].Scene} className="sceneImg"></img>
+            <img src={movies[0].Logo} className="sceneLogo"></img>
+            <p className="sceneDesc">{movies[0].Description}</p>
+            <div className='playButton'>
+            <Button
+              icon="chevronRightX2"
+              text="Play"
+              theme="secondary"
+              type="button"
+            />
+            <Button
+              icon="plus"
+              text="Add to My List"
+              theme="translucent"
+              type="button"
+            />
+            </div>
+          </div>
+
+        </Tab>
+        <Tab tabKey={2} tabName={"Series"} isDisabled={true}></Tab>
+        <Tab tabKey={3} tabName={"MyList"}></Tab>
+      </TabList>
+    </div>
   </>
 )
 }
